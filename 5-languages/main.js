a6cccdd71a06659d4550f77bd260c061aa7adf09
umokee <hituaev@gmail.com>
2026-09-13 20:02:08 +1000

    languages add

---
+++ 5-languages/main.js
@@ -0,0 +1,21 @@
+const language = "en";
+
+switch (language) {
+  case "en":
+    console.log("Good day!");
+    break;
+  case "en":
+    console.log("Добрый день!");
+    break;
+  case "de":
+    console.log("Gutten tag!");
+    break;
+  case "fr":
+    console.log("Bonne journée!");
+    break;
+  case "pl":
+    console.log("Dzień dobry!");
+    break;
+  default:
+    console.log("Language doesn't support");
+}
